a8a99b82e7f429b5a4cb037877575e27a23e8dd5
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    sm6250-common: Allow to access bootable/deprecated-ota targets
    Signed-off-by: Niranjan BR <niranjankannan2003@gmail.com>
    Signed-off-by: ihsan <ihsanulrahman@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -266,6 +266,7 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/bliss/interfaces/power-libperfmgr \
