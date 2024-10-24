34279f2761dcc2112b1ea71d083992e6848bd9b2
IHSAN <ihsanulrahman@gmail.com>
2024-10-25 01:28:01 +0500

    sm6250-common: Blissify

--- atoll.mk
+++ atoll.mk
@@ -177,17 +177,12 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
+    SM6250Aperture \
     SM6250Frameworks \
     SM6250Settings \
     SM6250SystemUI \
     TelephonyResCommon \
     WifiOverlay
-
-PRODUCT_PACKAGES += \
-    SM6250Aperture \
-    SM6250LineageDialer \
-    SM6250LineageSDK \
-    SM6250LineageSettings
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
@@ -277,7 +272,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/bliss/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi
 
