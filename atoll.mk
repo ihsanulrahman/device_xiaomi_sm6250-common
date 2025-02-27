581b912ffb2b90d4b8b47e8acbd4961db5fcb24f
Michael Bestas <mkbestas@lineageos.org>
2025-02-28 01:21:09 +0200

    sm6250-common: Enable UFFD GC
    Kernel has support for it now.
    
    Change-Id: I3863ee71477f321fe47646571b857fdc39a1902b
    Signed-off-by: iHSAN <ihsanulrahman@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -124,6 +124,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
+# Kernel
+PRODUCT_ENABLE_UFFD_GC := true
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
