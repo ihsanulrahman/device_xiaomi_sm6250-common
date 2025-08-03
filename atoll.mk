044bb7f586d8023ee67f6aa79b1e2013c9e98597
iHSAN <ihsanulrahman@gmail.com>
2025-08-03 15:05:42 +0500

    Revert "sm6250-common: Switch to NCM USB tethering"
    This reverts commit 397828e9be17d7ea3042832999a382e7539adddd.

--- atoll.mk
+++ atoll.mk
@@ -159,7 +159,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     SM6250Aperture \
-    NcmTetheringOverlay \
     SM6250DeviceAsWebcam \
     SM6250Frameworks \
     SM6250Settings \
