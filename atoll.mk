b736671227d1350995d080095d31f26c594be16c
iHSAN <ihsanulrahman@gmail.com>
2025-08-05 16:39:09 +0500

    sm6250-common: SQUASHED: switch back to schedtune
    Revert "sm6250-common: powerhint: Switch to uclamp boosting"
    
    This reverts commit 468a1ef02e60c7529b6d630f513d03ab3c877c7d.
    
    Revert "sm6250-common: Use foreground cpuset/uclamp for gralloc"
    
    This reverts commit 287c380feaf995d255f6fa1a5a89903c70d0610d.
    
    Revert "sm6250-common: Switch hwcomposer to foreground CPU set"
    
    This reverts commit ec164621eed4be900858dc6e2eb44be92faa8e12.
    
    Revert "sm6250-common: Remove frozen and unfrozen cgroup"
    
    This reverts commit 6c5c1b5a52cdc633b0f6e8b684d505006bfafe4f.
    
    Revert "sm6250-common: Import task_profiles.json"
    
    This reverts commit 0ce41fe0ab48c1f3bbc6fd9611f1c7b40b0bd411.
    
    Revert "sm6250-common: Switch from schedtune to uclamp"
    
    This reverts commit 9a39b790b81874817d52f759865bb83fa04a9491.

--- atoll.mk
+++ atoll.mk
@@ -228,8 +228,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.lineage-libperfmgr
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
-    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
 # QTI fwk-detect
 PRODUCT_PACKAGES += \
