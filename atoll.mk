b5f7abd22cf6debe9e4d102d8b3bc009f1254d50
iHSAN <ihsanulrahman@gmail.com>
2025-09-20 12:00:14 +0500

    sm6250-common: Drop msm_irqbalance
    * As we are using SBalance in kernel so msm_irqbalance won't be needdd.
    
    Signed-off-by: iHSAN <ihsanulrahman@gmail.com>

--- atoll.mk
+++ atoll.mk
@@ -124,10 +124,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.ir-service.example
 
-# IRQ
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # Kernel
 PRODUCT_ENABLE_UFFD_GC := true
 
